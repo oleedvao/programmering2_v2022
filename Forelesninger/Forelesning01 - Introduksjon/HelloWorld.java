93ff59ef77c8b3b51888cecb2a7b1a8dc1f85312
Ole-Edvard Ørebæk <oleedvao@hiof.no>
2022-01-11 10:31:47 +0100

    Uploaded files for lecture 1

---
+++ Forelesninger/Forelesning01 - Introduksjon/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+Klassedefinisjon.
+En klasse må defineres for enhver .java fil og må
+hete det samme som filnavnet; I dette tilfellet, HelloWorld.
+*/
+public class HelloWorld {
+
+    /*
+    Klassens main-metode.
+    Innholdet av main-metoden definerer hva som kodesekvensen ved kjøring av applikasjonen.
+    Denne må være inkludert og definert på følgende måte
+    for at en applikasjon skal bygge og kjøre.
+     */
+    public static void main(String[] args) {
+        // Skriver ut "Hello World!" til output
+        System.out.println("Hello World!");
+    }
+}
